624f3dcb4b560f2806da875f2fcef0f17e114c57
Sheldor05 <suryaagarwal.00@gmail.com>
2021-07-08 15:15:01 +0530

    Q2 leet code

---
+++ Arrays/Kids With the Greatest Number Candies.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int max {}, n = candies.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        if ( candies[i]> max)
+        {
+            max = candies[i];
+        }
+    }
+    vector<bool> result (n);
+    for ( int i = 0; i < n; i++)
+    {
+        result[i] = (candies [i] + extraCandies >= max);
+
+    }
+        return result;
+
+    }
+    /*int main() {
+        
+        int n;
+        int extraCandies;
+        cin >> n;
+        cin >> extraCandies;
+
+        vector <int> candies(n);
+        for(int i = o; i < candies.size(); i++)
+        {
+            can.push_back();
+        }
+        for (bool x: kidsWithCandies(candies, extraCandies))
+        cout << (x ? "true" : "false") << " ";
+        return 0 ;
+    }
+};*/
