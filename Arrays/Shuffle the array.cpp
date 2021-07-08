c26871017018415b197903c12d37735cd6a80b0f
Sheldor05 <suryaagarwal.00@gmail.com>
2021-07-09 01:38:16 +0530

    using vector push back

--- Arrays/Shuffle the array.cpp
+++ Arrays/Shuffle the array.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector <int> result;
+        vector <int> result; //new vector
         for ( int i = 0; i < n; i++){
             result.push_back(nums[i]);
             result.push_back(nums[i+n]);
