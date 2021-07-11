731e67c9fbe2eb124990730550e2565f56618aea
Sheldor05 <suryaagarwal.00@gmail.com>
2021-07-12 01:36:43 +0530

    1 solution is faster than 2

--- Arrays/Number of Good Pairs.cpp
+++ Arrays/Number of Good Pairs.cpp
@@ -1,3 +1,16 @@
+/*Given an array of integers nums.
+
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+Return the number of good pairs.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -13,3 +26,15 @@ public:
     }
     
 }
+// 2 solution
+lass Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int res = 0;
+unordered_map<int,int> count;
+for (auto i : nums)
+res += count[i]++;
+        return res;
+
+    }
+};
