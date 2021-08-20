ecd67fbc76391f3e30964fe700f585f8a44a5cdf
Sheldor05 <suryaagarwal.00@gmail.com>
2021-08-20 19:17:17 +0530

    done

---
+++ Arrays/Decompress RLE list.cpp
@@ -0,0 +1,18 @@
+// We are given a list nums of integers representing a list compressed with run-length encoding.
+
+// Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+
+// Return the decompressed list.
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+          vector<int> v;
+        for ( int i = 0;i<nums.size();i+=2)
+            for(int j=0;j<nums[i];j++)
+                v.push_back(nums[i+1]);
+           return v;  
+    }
+    
+};
